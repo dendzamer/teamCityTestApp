2744fc32bbd9da65d79bb16c379a80a0c396836a
RYSTADENERGY\DenisM <denis.merdzic@rystadenergy.com>
2024-04-11 16:28:36 +0200

    "something new"

--- teamCityTest/Controllers/WidgetsController.cs
+++ teamCityTest/Controllers/WidgetsController.cs
@@ -10,6 +10,7 @@ public class WidgetsController : ControllerBase
     private readonly IMongoCollection<Widget> widgets;
     //some comment
     //some other new change
+    // new new trigger
     public WidgetsController(WidgetContext context)
     {
         widgets = context.WidgetSet;
